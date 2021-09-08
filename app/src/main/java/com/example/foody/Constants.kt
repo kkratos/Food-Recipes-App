cb5a22b61e5df9b0f6509eaf2fcf144401aefb5f
Kamal Patel <kamal.patel30@hotmail.com>
2021-09-08 22:58:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/foody/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.foody
+
+class Constants {
+    companion object{
+
+        const val BASE_URL = "https://api.spoonacular.com/"
+        const val API_KEY="0c41c00c712640b6a81863bd28ac4189"
+    }
+}
